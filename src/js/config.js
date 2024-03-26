d42e92ae5e1a0f762ff681e8d042f55a50ede461
Mitatsej <mitatsejdiu18@gmail.com>
2024-03-26 14:32:42 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "fd6f898b-634c-4a66-8efa-688562fda641";
+export const MODAL_CLOSE_SEC = 2.5;
